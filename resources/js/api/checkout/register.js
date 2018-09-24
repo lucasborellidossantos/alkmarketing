c0d7dcd8de68cdd2a78d645595f0a412de176805
Lucas Borelli <lucas@imaxinformatica.com.br>
2018-09-24 08:58:08 -0300

    Segunda avaliação

---
+++ resources/js/api/checkout/register.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+
+    $(".cpf_cnpj_checkout").change(function () {
+        let field = $(this).val().toString();
+
+        if (field.length > 14) {
+            $('.ie_checkout').trigger({ type: 'keypress', which: 13, keyCode: 13 });
+        } else {
+            $('.rg_checkout').trigger({ type: 'keypress', which: 13, keyCode: 13 });
+        }      
+    });
+
+    $.jetRoute("checkout", function () {
+        $("#checkoutRegister input").keypress(function (e) {
+            var code = null;
+            code = (e.keyCode ? e.keyCode : e.which);
+            return (code == 13) ? false : true;
+        });
+    });
+
+    $("#checkoutRegister").keydown(function (event) {
+        if (event.keyCode == 10 || event.keyCode == 13) {
+            event.preventDefault();
+            return false;
+        }
+    });
+});
+
+/*
+
+
+
+*/
